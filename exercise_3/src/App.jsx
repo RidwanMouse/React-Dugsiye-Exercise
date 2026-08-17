b473e00ed3dbdd1f0ee88412871fefbe6a267d9a
RidwanMouse <hajiridwan4028@gmail.com>
2026-08-17 19:04:24 +0300

    added exercise_3

---
+++ exercise_3/src/App.jsx
@@ -0,0 +1,14 @@
+
+import Usercard from "./Usercard";
+
+function App() {
+  return (
+    <>
+      <Usercard name="Ridwan" email="Ridwan@gmail.com" />
+      <Usercard name="ahmed" email="ahmed@gmail.com" />
+      <Usercard name = "Abdirahman" email= "abdirah@gmail.com"/>
+    </>
+  );
+}
+
+export default App;
